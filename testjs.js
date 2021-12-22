d74fb4c49c3be391c6b3aa1387e3fc1efe24cca7
jbennettccsi <jbennett@ccsinetworks.com>
2021-12-22 10:45:26 -0600

    Street View

--- testjs.js
+++ testjs.js
@@ -1,47 +1,19 @@
-require([
-      "esri/portal/Portal",
-      "esri/identity/OAuthInfo",
-      "esri/identity/IdentityManager"
-    ], function (Portal, OAuthInfo, esriId) {
+function initialize() {
+  const fenway = { lat: 42.345573, lng: -71.098326 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: fenway,
+    zoom: 14,
+  });
+  const panorama = new google.maps.StreetViewPanorama(
+    document.getElementById("pano"),
+    {
+      position: fenway,
+      pov: {
+        heading: 34,
+        pitch: 10,
+      },
+    }
+  );
 
-      const info = new OAuthInfo({
-        appId: "q3yu8yxxzdbg5Grz",
-        popup: false // the default
-      });
-      esriId.registerOAuthInfos([info]);
-
-      esriId
-        .checkSignInStatus(info.portalUrl + "/sharing")
-        .then(() => {
-          handleSignedIn();
-        })
-
-        .catch(() => {
-          handleSignedOut();
-
-        });
-
-      document.getElementById("sign-in").addEventListener("click", function () {
-        esriId.getCredential(info.portalUrl + "/sharing");
-      });
-
-      document.getElementById("sign-out").addEventListener("click", function () {
-        esriId.destroyCredentials();
-        window.location.reload();
-      });
-
-      function handleSignedIn() {
-
-        const portal = new Portal();
-        portal.load().then(() => {
-          const results = { name: portal.user.fullName, username: portal.user.username };
-          document.getElementById("results").innerText = JSON.stringify(results, null, 2);
-        });
-
-      }
-
-      function handleSignedOut() {
-        document.getElementById("results").innerText = 'Signed Out'
-      }
-
-    });
+  map.setStreetView(panorama);
+}
