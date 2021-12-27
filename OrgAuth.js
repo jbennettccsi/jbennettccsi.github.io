2d9a222e881267d75b3f06b496ba9be6b0255718
jbennettccsi <jbennett@ccsinetworks.com>
2021-12-27 13:34:42 -0600

    Org Auth

--- OrgAuth.js
+++ OrgAuth.js
@@ -1,16 +1,44 @@
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+  require([
+    "esri/config",
+    "esri/Map",
+    "esri/views/MapView",
 
-	esriConfig.apiKey = "q3yu8yxxzdbg5Grz";
+    "esri/layers/FeatureLayer"
 
-const map = new Map({
-  basemap: "arcgis-topographic" // Basemap layer service
-});
+  ], function(esriConfig,Map, MapView, FeatureLayer) {
 
-const view = new MapView({
-  map: map,
-  center: [-118.805, 34.027], // Longitude, latitude
-  zoom: 13, // Zoom level
-  container: "viewDiv" // Div element
-});
+  esriConfig.apiKey = "q3yu8yxxzdbg5Grz";
 
-});
+  const map = new Map({
+    basemap: "arcgis-topographic"
+  });
+
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-87.747286,41.963653],
+    zoom: 13
+  });
+
+//Trailheads feature layer (points)
+  const trailheadsLayer = new FeatureLayer({
+    url: "https://services6.arcgis.com/uUNS2ciCcjNBnb3f/arcgis/rest/services/NED_Small_Cell/FeatureServer/0"
+  });
+
+  map.add(trailheadsLayer);
+
+//Trails feature layer (lines)
+  const trailsLayer = new FeatureLayer({
+    url: "https://services6.arcgis.com/uUNS2ciCcjNBnb3f/arcgis/rest/services/NED_Fiber_Routes/FeatureServer/0"
+  });
+
+  map.add(trailsLayer, 0);
+
+// Parks and open spaces (polygons)
+  const parksLayer = new FeatureLayer({
+    url: "https://services6.arcgis.com/uUNS2ciCcjNBnb3f/arcgis/rest/services/Area_Notes/FeatureServer/0"
+  });
+
+  map.add(parksLayer, 0);
+
+  });
