43d4d59ce64d68ac7ecdbe607d7b96930fb12120
jbennettccsi <jbennett@ccsinetworks.com>
2021-12-28 09:06:26 -0600

    Update OrgAuth.js

--- OrgAuth.js
+++ OrgAuth.js
@@ -1,20 +1,15 @@
-
-
-//AAPKcb4e9f06e3e14bc087a75d0cd0e90ed8E9Fp-hjVMRneNabPlLRw_NvneSA8LuafesQ25Rk5zTb2tQFjJVgPPH7bhAP3eltH
-
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+require(["esri/config","esri/Map","esri/views/MapView"], function(esriConfig,Map,MapView) {
 
     esriConfig.apiKey = "AAPKcb4e9f06e3e14bc087a75d0cd0e90ed8E9Fp-hjVMRneNabPlLRw_NvneSA8LuafesQ25Rk5zTb2tQFjJVgPPH7bhAP3eltH";
 
-    const map = new Map({
-      basemap: "arcgis-charted-territory" // Basemap layer service
-    });
+    const map = new Map({basemap: "arcgis-charted-territory"});
 
     const view = new MapView({
-      map: map,
-      center: [-118.805, 34.027], // -87.747 41.963
-      zoom: 13, // Zoom level
-      container: "viewDiv" // Div element
+        map: map,
+        center: [-118.805, 34.027],  //-87.747 41.963
+        zoom: 13,
+        container: "viewDiv"  //This is the div in the HTML doc that will show the map
     });
-
 });
+
+//AAPKcb4e9f06e3e14bc087a75d0cd0e90ed8E9Fp-hjVMRneNabPlLRw_NvneSA8LuafesQ25Rk5zTb2tQFjJVgPPH7bhAP3eltH
