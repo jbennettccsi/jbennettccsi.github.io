29c009215655cf70332c9552130064013008453c
jbennettccsi <jbennett@ccsinetworks.com>
2021-12-28 09:33:10 -0600

    Update OrgAuth.js

--- OrgAuth.js
+++ OrgAuth.js
@@ -13,30 +13,34 @@ require([
     basemap: "arcgis-topographic"
   });
 
-        const view = new MapView({
-            map: map,
-            center: [-118.805, 34.027],  //-87.747 41.963
-            zoom: 13,
-            container: "viewDiv"  //This is the div in the HTML doc that will show the map
-        });
+  const view = new MapView({
+    container: "viewDiv",
+    map: map,
+    center: [-118.80543,34.02700],
+    zoom: 13
+  });
 
-        //Points
-        const trailheadsLayer = new FeatureLayer({
-            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
-        });        
-        map.add(trailheadsLayer); //command will add thing to map
+//Trailheads feature layer (points)
+  const trailheadsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+  });
 
-        //Polylines
-        const trailsLayer = new FeatureLayer({
-            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
-        });        
-        map.add(trailsLayer, 0);
+  map.add(trailheadsLayer);
 
-        //Polygons
-        const parksLayer = new FeatureLayer({
-            url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
-        });        
-        map.add(parksLayer, 0);
-});
+//Trails feature layer (lines)
+  const trailsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+  });
+
+  map.add(trailsLayer, 0);
+
+// Parks and open spaces (polygons)
+  const parksLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
+  });
+
+  map.add(parksLayer, 0);
+
+  });
 
 //AAPKcb4e9f06e3e14bc087a75d0cd0e90ed8E9Fp-hjVMRneNabPlLRw_NvneSA8LuafesQ25Rk5zTb2tQFjJVgPPH7bhAP3eltH
