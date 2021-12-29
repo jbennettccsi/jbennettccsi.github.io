ff0728447d795cf13005e5215d2c083fa5126478
jbennettccsi <jbennett@ccsinetworks.com>
2021-12-29 14:38:29 -0600

    OAuth attempt

---
+++ OAuth2.js
@@ -0,0 +1,40 @@
+require([
+    "esri/portal/Portal",
+    "esri/identity/OAuthInfo",
+    "esri/identity/IdentityManager",
+    "esri/portal/PortalQueryParams"
+    ],
+    function(Portal, OAuthInfo, IdentityManager, PortalQueryParams){
+
+        const oAuthInfo = new OAuthInfo({
+            appId: "q3yu8yxxzdbg5Grz",  //app ID
+            popup: false  //false causes redirect to Arcgis.com, then back to app
+        });
+        esriId.registerOAuthInfos([oAuthInfo]);
+
+        //Check to see if user is logged in
+        esriId.checkSignInStatus(info.portalUrl + "/sharing").then(() => {
+            displayItems();
+        }).catch(/*code that allows user to sign in*/);
+
+        //Retrieve user credentials
+        esriId.getCredential(info.portalUrl + "/sharing");
+
+        //Run this to destroy credentials (essentially sign out)
+        //esriId.destroyCredentials();
+
+        //Create portal
+        const portal = new Portal();
+        portal.authMode = "immediate";  //signs user in when loaded??
+        portal.load().then(() => {
+            //Query portal for items to display
+            const portalQueryParams = new PortalQueryParams({
+                query: "owner:" + portal.user.username,
+                sortField: "numViews",
+                sortOrder: "desc",
+                num: 100
+            });
+            portal.queryItems(portalQueryParams).then(createGallery);
+        });
+    }
+);
